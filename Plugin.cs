7d9bd0836025dab2e6b7cda13b27205f7879bdc9
ZedDevStuff <kouamejuniorauguste@gmail.com>
2023-11-14 16:00:50 +0000

    Ajoutez des fichiers projet.

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using UnityEngine.SceneManagement;
+
+namespace USTManager
+{
+    [BepInPlugin("zed.uk.ustmanager", "USTManager", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
